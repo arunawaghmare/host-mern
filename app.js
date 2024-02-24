d87d5c3a1f2bcd0fcf7442da1b67b077901572c6
arunawaghmare <53028975+arunawaghmare@users.noreply.github.com>
2024-02-24 21:09:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.use
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use('/api/v1/reservation',router);
+app.use("/api/v1/reservation", reservationRouter);
+app.get("/", (req, res, next)=>{return res.status(200).json({
+  success: true,
+  message: "HELLO WORLD AGAIN"
+})})
 
 
 
